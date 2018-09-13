fa0e7ca84d65e267c0bdea2a37b1149d106e378f
Jimmy <jgs2017@gmail.com>
2018-09-13 11:31:46 -0500

    add gatsby-plugin-jss

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-lodash",
     "gatsby-plugin-sass",
+    "gatsby-plugin-jss",
     {
       resolve: "gatsby-source-filesystem",
       options: {
